014a4968a521b0e61ada17253ee69d8ed04fb181
Lalitha <lalitha.d339@gmail.com>
2023-07-25 11:03:14 +0530

    shell scripts

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-history
+echo hi
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m  FAILURE \e[0m"
+  fi
+  echo hii
+   if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+   else
+    echo -e "\e[31m FAILURE \e[0m"
+   fi
+echo hiiiiii                                                                                                                                                                                                                                                                                                        
